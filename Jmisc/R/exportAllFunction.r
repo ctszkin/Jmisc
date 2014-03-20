8e23a3becd102d00deb3f0a4fefc07fede380b24
TszKin Julian Chan <ctszkin@gmail.com>
2014-03-19 20:38:46 -0400

    remove Jmisc/ from .gitignore

---
+++ Jmisc/R/exportAllFunction.r
@@ -0,0 +1,20 @@
+#' Export all function in an environment to a cluster
+#' @name sourceAll
+#' @aliases sourceAll
+#' @title Source all the R files of a directory
+#' @param cl Target cluster
+#' @param envir environment of functions. Default is .GlobalEnv
+#' @author TszKin Julian Chan \email{ctszkin@@gmail.com}
+#' @seealso \link{repCol}
+#' @export
+#' @examples
+#' \dontrun{   
+#' cl = makeCluster(2)
+#' exportAllFunction(cl)
+#' }
+exportAllFunction <- function(cl, envir=.GlobalEnv){
+  all_stuff = ls(envir=envir )
+  is_fun = sapply(lapply(all_stuff,get), is.function)
+  clusterExport(cl,all_stuff[is_fun])
+}
+
